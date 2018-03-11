1b219114b2aa8f2a56d19e843d42f61bd3d3badf
Azmath Shamrad <azmath_shamrad@yahoo.com>
2018-03-11 10:01:16 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace sqlExpressDB_backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+
+            // read backup folder from config file ("C:/temp/")
+            var backupFolder = ConfigurationManager.AppSettings["BackupFolder"];
+
+            var sqlConStrBuilder = new SqlConnectionStringBuilder(connectionString);
+
+            // set backupfilename (you will get something like: "C:/temp/MyDatabase-2013-12-07.bak")
+            var backupFileName = String.Format("{0}{1}-{2}.bak",
+                backupFolder, sqlConStrBuilder.InitialCatalog,
+                DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
+
+            using (var connection = new SqlConnection(sqlConStrBuilder.ConnectionString))
+            {
+                var query = String.Format("BACKUP DATABASE {0} TO DISK='{1}'",
+                    sqlConStrBuilder.InitialCatalog, backupFileName);
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
